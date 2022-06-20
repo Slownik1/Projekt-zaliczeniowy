14f91e2079886989159fdebddf75d97c894bc639
dawid białek <dawid1bialek@gmail.com>
2022-06-20 17:59:17 +0200

    Initial commit

---
+++ src/main/java/com/example/Projekt/zaliczeniowy/ProjektZaliczeniowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projekt.zaliczeniowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZaliczeniowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZaliczeniowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projekt/zaliczeniowy/ProjektZaliczeniowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projekt.zaliczeniowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZaliczeniowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
